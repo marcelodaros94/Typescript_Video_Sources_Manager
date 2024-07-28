e0352e0d876f0483190dc7a07321633305bf5e07
Marcelo Da Ros <marcelodaros94@gmail.com>
2024-07-28 10:49:59 -0500

    open-closed

---
+++ src/interfaces/videoServiceInterface.ts
@@ -0,0 +1,6 @@
+import { Video } from "../redux/types";
+
+export interface IVideoService {
+    getVideos(page: number, term: string, rating: number): Promise<{ data: { videos: Video[], totalPages: number } }>;
+}
+  
